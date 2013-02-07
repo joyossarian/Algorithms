7ef01845827fd3c70c0d045ac7c292e7d4e3ff54
jotiger <joyossarian@gmail.com>
2013-02-07 14:53:38 +0800

    Update leetcode/UniquePaths.cpp

--- leetcode/UniquePaths.cpp
+++ leetcode/UniquePaths.cpp
@@ -3,11 +3,8 @@ public:
     int uniquePaths(int m, int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int p1 = m - 1;
-        int p2 = n - 1;
-        int p3 = (p1 + p2);
         double result = 1.0;
-        for (int i = p1 + 1, j = 1; i <= p1 + p2; ++i, ++j)
+        for (int i = m, j = 1; i <= m + n - 2; ++i, ++j)
         {
             result *= (double)(i) / (double)j;
         }
